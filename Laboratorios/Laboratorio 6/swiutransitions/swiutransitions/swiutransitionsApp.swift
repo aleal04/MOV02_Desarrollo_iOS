a71594e25c92452cf2d8fbce42593e8247a8dedd
aleal04 <46423443+aleal04@users.noreply.github.com>
2021-09-04 10:27:21 -0600

    Laboratorio #6

---
+++ Laboratorios/Laboratorio 6/swiutransitions/swiutransitions/swiutransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiutransitionsApp.swift
+//  swiutransitions
+//
+//  Created by Andrey Leal on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct swiutransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
