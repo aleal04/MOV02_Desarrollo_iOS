d2bdd23e3dc846ce65707b54cd4e1dd1225b5383
aleal04 <46423443+aleal04@users.noreply.github.com>
2021-09-18 09:52:06 -0600

    Laboratorio 10

---
+++ Laboratorios/Laboratorio 10/cocoapods/cocoapods/cocoapodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocoapodsApp.swift
+//  cocoapods
+//
+//  Created by Andrey Leal on 18/9/21.
+//
+
+import SwiftUI
+
+@main
+struct cocoapodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
