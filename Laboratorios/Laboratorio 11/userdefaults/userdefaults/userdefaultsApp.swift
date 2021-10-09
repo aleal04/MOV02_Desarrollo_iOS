35f396dcb7ede636b1b69d91788f4faa85100be6
aleal04 <46423443+aleal04@users.noreply.github.com>
2021-10-09 10:11:36 -0600

    Laboratorio 11

---
+++ Laboratorios/Laboratorio 11/userdefaults/userdefaults/userdefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  userdefaultsApp.swift
+//  userdefaults
+//
+//  Created by Andrey Leal on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct userdefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
